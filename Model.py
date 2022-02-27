22d36252a4ef26b6eea1be7816c9f4b3ec94d8a0
Michael <mwellblack@gmail.com>
2022-02-26 22:37:39 -0500

    Working on FCN

--- Model.py
+++ Model.py
@@ -3,19 +3,15 @@ import tensorflow_addons as tfa
 import tensorboard
 
 timestamp = datetime.now().strftime('%m-%d-%Y_%H.%M.%S')
-# width, height, depth
-image_shape = (1024, 1024, 3)
-train_path = Path('/media/storage/RSNA Brain Tumor Project/train_tr')
-val_path = Path('/media/storage/RSNA Brain Tumor Project/val_tr')
 
 
-def build_model(width, height, depth, name='Model'):
+def build_model(image_size, name='Model'):
     """Build a 3D convolutional neural network model
     There are many ways to do this."""
 
-    image = tf.keras.Input(shape=(width, height, depth), name='Input')
+    image = tf.keras.Input(shape=image_size, name='image')
 
-    kernelsize = (11, 11, 3)
+    kernelsize = 1
     filters = 32
 
     # Convolution
@@ -25,39 +21,36 @@ def build_model(width, height, depth, name='Model'):
     x = tf.keras.layers.BatchNormalization()(x)
     x = tf.keras.layers.Activation('relu')(x)
 
-
     # Convolve again
-    x = tf.keras.layers.Conv3D(filters=2, kernel_size=1, activation='relu')(x)
+    x = tf.keras.layers.Conv2D(filters=2, kernel_size=1, activation='relu')(x)
     # x = tf.keras.layers.MaxPool3D(pool_size=2)(x)
     x = tf.keras.layers.Dropout(0.2)(x)
     x = tf.keras.layers.BatchNormalization()(x)
     mask = tf.keras.layers.GlobalMaxPooling2D()(x)
-    mask = tf.keras.layers.Activation('softmax')(mask)
+    mask = tf.keras.layers.Activation('softmax', name='mask')(mask)
     # TODO implement mask output
     # Flatten, Dense, Output
-    x = tf.keras.layers.Flatten()(x)
-    x = tf.keras.layers.Dense(units=50, activation="relu")(x)
-    melanoma = tf.keras.layers.Dense(units=1, activation='sigmoid')(x)
-    keratosis = tf.keras.layers.Dense(units=1, activation='sigmoid')(x)
+    # x = tf.keras.layers.Flatten()(x)
+    # x = tf.keras.layers.Dense(units=50, activation="relu")(x)
+    # melanoma = tf.keras.layers.Dense(units=1, activation='sigmoid', name='melanoma_label')(x)
+    # keratosis = tf.keras.layers.Dense(units=1, activation='sigmoid', name='keratosis_label')(x)
 
     # Define the model.
-    model = tf.keras.Model(inputs=[image], outputs=[mask, melanoma, keratosis], name=name)
+    model = tf.keras.Model(inputs=[image], outputs=[mask], name=name) #melanoma, keratosis], name=name)
 
     # Compile model
     model.compile(
-        loss=tfa.losses.sigmoid_focal_crossentropy,
+        loss='categorical_crossentropy',  # tfa.losses.sigmoid_focal_crossentropy,
         optimizer='Adam',  # tf.keras.optimizers.Adam(learning_rate=lr_schedule),
-        metrics=["binary_accuracy"],
+        metrics=["accuracy"],
     )
 
     return model
 
 
-def train_model(train, batch: int, epochs: int):
+def train_model(data_pipe, epochs: int):
     # Build the model
-    model = build_model(width=image_shape[0],
-                        height=image_shape[1],
-                        depth=image_shape[2])
+    model = build_model(data_pipe.image_size)
 
     # Define callbacks and create folders to save them in
     tb_path = Path.cwd() / f'Callbacks/tensorboard/{timestamp}'
@@ -69,40 +62,23 @@ def train_model(train, batch: int, epochs: int):
     early_stop_path.mkdir()
 
     tensorboard_cb = tf.keras.callbacks.TensorBoard(log_dir=str(tb_path),
-                                                    # write_images=True,
+                                                    write_images=True,
                                                     histogram_freq=1,
                                                     )
     checkpoint_cb = tf.keras.callbacks.ModelCheckpoint(filepath=str(ckpt_path), save_best_only=True)
-    early_stopping_cb = tf.keras.callbacks.EarlyStopping(monitor="binary_accuracy", patience=15)
+    early_stopping_cb = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=15)
 
     # Launch Tensorboard, can be accessed by going to http://localhost:6006 in your browser
     tb = tensorboard.program.TensorBoard()
     tb.configure(argv=[None, '--logdir', str(tb_path)])
     url = tb.launch()
 
-    # Training data input generator
-    train_gen = DataPipe(filepath=train_path)
-    training_data = tf.data.Dataset.from_generator(train_gen.input_generator,
-                                                   output_types=((tf.float32, tf.float32, tf.float32, tf.float32),
-                                                                 tf.int64),
-                                                   output_shapes=((image_shape, image_shape, image_shape, image_shape),
-                                                                  (1, 1)))
-    # validation data input generator
-    val_gen = DataPipe(filepath=val_path)
-    validation_data = tf.data.Dataset.from_generator(val_gen.input_generator,
-                                                     output_types=((tf.float32, tf.float32, tf.float32, tf.float32),
-                                                                   tf.int64),
-                                                     output_shapes=(
-                                                         (image_shape, image_shape, image_shape, image_shape),
-                                                         (1, 1)))
-
     # Change to 'CPU:0' to use CPU instead of GPU
     with tf.device('GPU:0'):
         model.fit(
-            training_data.batch(batch),  # input_generator(train_path, scan_type, 1),#
-            validation_data=validation_data.batch(batch),  # input_generator(val_path, scan_type, 1),   #
+            data_pipe.train,  # input_generator(train_path, scan_type, 1),#
+            validation_data=data_pipe.val,  # input_generator(val_path, scan_type, 1),   #
             epochs=epochs,
-            batch_size=batch,
             # shuffle=True,
             # verbose=2,
             callbacks=[checkpoint_cb, early_stopping_cb, tensorboard_cb]
@@ -110,6 +86,8 @@ def train_model(train, batch: int, epochs: int):
 
     # save model
     model.save(f'./models/All_Scans{timestamp}')
+    # TODO run and save metrics
+    # TODO run test data
 
     # show metrics
     # fig, ax = plt.subplots(1, 2, figsize=(20, 3))
@@ -127,6 +105,8 @@ def train_model(train, batch: int, epochs: int):
 
 
 if __name__ == '__main__':
-    train_model(train_path, val_path, batch=2, epochs=10)
+    dataset = DataPipe(batch=5)
+    dataset.transform_all()
+    train_model(dataset, epochs=10)
 
 
